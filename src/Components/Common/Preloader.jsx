560be521ab70aeb8b9a937ac17aedd59df58c7b9
Naba88Sehrish <naba55sehrish@gmail.com>
2024-06-15 02:01:01 -0500

    Update preloader

--- src/Components/Common/Preloader.jsx
+++ src/Components/Common/Preloader.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Preloader = () => {
     return (
-        <div className='flex items-center justify-center h-full fixed inset-0   z-[99999]  bg-cover bg-center' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Assets/Images/gbt1.jpg)` }}>
-               
-            
+       
+        <div className='flex items-center justify-center h-screen fixed inset-0 bg-[#221D1A]/90 backdrop-blur-md z-[99999]'>
+            <div className='flex items-center'>
+                <img className='h-screen object-contain' src={`${process.env.PUBLIC_URL}/Assets/Images/mg.png`} alt='logo'/>
+            </div>
         </div>
-
        
    
 
