fbf73e8b235b8aa2bd18b51ddfeddfe05ce35be6
agha-0 <ahmedtayyab533@gmail.com>
2024-06-24 22:08:35 +0500

    remove advertisment text

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,68 +8,67 @@ import Banner from "../../Components/Common/Banner";
 const Home = () => {
   return (
     <>
-    <Layout customContainer={"mx-auto w-[95%]"}>
-      <Banner
-        backgroundImage={"/Assets/Images/bg.png"}
-        customMargin={"flex items-center justify-start md:pl-48 xs:pl-24 pl-4"}
-        data={
-          <>
-            <ul className="mt-4">
-              <li className="flex font-bold md:text-[2rem]  sm:text-[1.5rem] text-[1.2rem] items-center">
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="text-#FBDB4A mr-4"
-                />
-                <a
-                  // href="/"
-                  className="no-underline hover:underline text-current"
-                >
-                  Ask Anything Medical
-                </a>
-              </li>
-              <li className="flex font-semibold md:text-[2rem] sm:text-[1.5rem] text-[1.2rem] items-center">
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="text-#FBDB4A mr-4"
-                />
-                <a
-                  // href="/"
-                  className="no-underline hover:underline text-current"
-                >
-                  Educational
-                </a>
-              </li>
-              <li className="flex font-semibold md:text-[2rem] sm:text-[1.5rem] text-[1.2rem] items-center">
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="text-#FBDB4A mr-4"
-                />
-                <a
-                  // href="/"
-                  className="no-underline hover:underline text-current"
-                >
-                  Verify Info
-                </a>
-              </li>
-              <li className="flex font-semibold md:text-[2rem] sm:text-[1.5rem] text-[1.2rem] items-center">
-                <FontAwesomeIcon
-                  icon={faArrowRight}
-                  className="text-#FBDB4A mr-4"
-                />
-                <a
-                  // href="/"
-                  className="no-underline hover:underline text-current"
-                >
-                  Not a Med Advice
-                </a>
-              </li>
-            </ul>
-          </>
-        }
-      />
-      <Search></Search>
-      <h1 className="sm:text-[60px] text-[25px] text-[#4d4d4d] mx-auto w-[90%] text-center py-10">Advertisment Comming Soon</h1>
-    </Layout>
+      <Layout customContainer={"mx-auto w-[95%]"}>
+        <Banner
+          backgroundImage={"/Assets/Images/bg.png"}
+          customMargin={"flex items-center justify-start md:pl-48 xs:pl-24 pl-4"}
+          data={
+            <>
+              <ul className="mt-4">
+                <li className="flex font-bold md:text-[2rem]  sm:text-[1.5rem] text-[1.2rem] items-center">
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className="text-#FBDB4A mr-4"
+                  />
+                  <a
+                    // href="/"
+                    className="no-underline hover:underline text-current"
+                  >
+                    Ask Anything Medical
+                  </a>
+                </li>
+                <li className="flex font-semibold md:text-[2rem] sm:text-[1.5rem] text-[1.2rem] items-center">
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className="text-#FBDB4A mr-4"
+                  />
+                  <a
+                    // href="/"
+                    className="no-underline hover:underline text-current"
+                  >
+                    Educational
+                  </a>
+                </li>
+                <li className="flex font-semibold md:text-[2rem] sm:text-[1.5rem] text-[1.2rem] items-center">
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className="text-#FBDB4A mr-4"
+                  />
+                  <a
+                    // href="/"
+                    className="no-underline hover:underline text-current"
+                  >
+                    Verify Info
+                  </a>
+                </li>
+                <li className="flex font-semibold md:text-[2rem] sm:text-[1.5rem] text-[1.2rem] items-center">
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className="text-#FBDB4A mr-4"
+                  />
+                  <a
+                    // href="/"
+                    className="no-underline hover:underline text-current"
+                  >
+                    Not a Med Advice
+                  </a>
+                </li>
+              </ul>
+            </>
+          }
+        />
+        <Search></Search>
+      </Layout>
     </>
   );
 };
